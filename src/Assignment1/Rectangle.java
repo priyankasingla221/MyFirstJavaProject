4a4b0b1a81470ab1e0c41305a1a597dbd503c2e4
Priyanka <priyankasingla221@gmail.com>
2021-12-17 16:40:47 -0400

    my second commit

--- src/Assignment1/Rectangle.java
+++ src/Assignment1/Rectangle.java
@@ -12,6 +12,7 @@ public class Rectangle {
 		System.out.println("Length = " +length+ "\nBreadth = " +breadth);
 		System.out.println("Perimeter of Rectangle is= " +perimeter);
 		System.out.println("Area of Rectangle is= " +area);
+		System.out.println("Testing GIT");
 		
 		
 	}
